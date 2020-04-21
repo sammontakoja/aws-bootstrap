9d5c9585091988797f8a004df7df50ec4ae539ef
Ari Aaltonen <ariaaltonen@pop-os.localdomain>
2020-04-22 00:26:04 +0300

    Implemented simple credit rating service

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
-const { hostname } = require('os');
-const http = require('http');
-const message = 'Hello World\n';
+const express = require('express');
+const {FinnishSSN} = require('finnish-ssn');
+const app = express();
+
+const pickRating = (ssn) => {
+    if (ssn === '020268-687L')
+        return 'A';
+    if (ssn === '050342-807H')
+        return 'B';
+    return 'C';
+};
+
+app.get('/:version/creditrating/:ssn', (req, res) => {
+    const handleResponse = () => {
+        if (FinnishSSN.validate(req.params.ssn)) {
+            const body = JSON.stringify({creditrating: pickRating(req.params.ssn)});
+            res.send(body);
+        } else {
+            res.status(400);
+            res.send('');
+        }
+    };
+    if (req.params.version === 'v1') {
+        handleResponse();
+    } else {
+        if (req.params.version === 'v2') {
+            if ((req.header('caller-id') !== 'ajv') ||
+                (req.header('caller-key') !== 'eW91cmJhc2VhcmViZWxvbmd0b3VzCg==')) {
+                res.status(401);
+                res.send('');
+            } else {
+                handleResponse();
+            }
+        } else {
+            res.status(404);
+            res.send('');
+        }
+    }
+});
+
 const port = 8080;
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end(message);
-});
-server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname()}:${port}/`);
-});
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
